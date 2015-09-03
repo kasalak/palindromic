019add8a46fb17d36e679a47008a76445abd81f6
Karthik Kasala <kasalak@gmail.com>
2015-09-02 22:13:31 -0400

    Tried to push, but could not. Copied into instead.

--- palindrome.py
+++ palindrome.py
@@ -1,1 +1,23 @@
+import re
 
+def is_palindrome(sentence):
+    # TODO: return True or False if the sentence is or isn't a palindrome
+
+    a = re.sub(r'[^A-Za-z]', "", sentence).lower()
+    b = a[::-1]
+
+    if a == b:
+        return True
+    else:
+        return False
+
+def main():
+    # TODO: put your input/output code here
+    sentence = input("Input text here: ")
+    if is_palindrome(sentence) == True:
+        print("'{}' is a palindrome.".format(sentence))
+    else:
+        print("'{}' is not a palindrome.".format(sentence))
+
+if __name__ == '__main__':
+    main()
